f46ed9fc4d2ce52f7bd0562233a9558eb2a971cd
wedesignstudios <deelden@gmail.com>
2017-03-09 11:18:11 -0600

    add knex; create app.up() db migration; change var -> const in ./index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const knex = require('knex');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const ENV = process.env.NODE_ENV || 'development';
+const config = require('./knexfile');
+const db = knex(config[ENV]);
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,12 +33,13 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');  
   err.status = 404;
   next(err);
 });
@@ -48,5 +54,16 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// migrate latest db models
+app.up = () => {
+  return db.migrate.latest([ENV])
+    .then(() => {
+      return db.migrate.currentVersion();
+    })
+    .then((val) => {
+      console.log('Done running latest migration:', val);      
+    });
+  }
 
 module.exports = app;

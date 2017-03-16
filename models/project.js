f7b82a406a6579dadcb75134857dcb8b97d55f84
wedesignstudios <deelden@gmail.com>
2017-03-16 17:48:33 -0500

    refactor Project model to use through() method to access join table

--- models/project.js
+++ models/project.js
@@ -3,15 +3,17 @@ const bookshelf = require('../db/bookshelf');
 const Client = require('./client');
 const Collaborator = require('./collaborator');
 const Image = require('./image');
+const ProjectsClients = require('./projects_clients');
+const ProjectsCollaborators = require('./projects_collaborators');
 
 const Project = bookshelf.Model.extend({
   tableName: 'projects',
   hasTimestamps: true,
   clients: function() {
-    return this.belongsToMany('Client', 'projects_clients', 'client_id');
+    return this.belongsToMany('Client').through('ProjectsClients');
   },
   collaborators: function() {
-    return this.belongsToMany('Collaborator', 'projects_collaborators', 'collaborator_id');
+    return this.belongsToMany('Collaborator').through('ProjectsCollaborators');
   },
   images: function() {
     return this.hasMany('Image');

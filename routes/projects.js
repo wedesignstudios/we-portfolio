4241d51f174c503d45f7e2f1970f61295263d107
wedesignstudios <deelden@gmail.com>
2017-03-16 17:55:28 -0500

    add params, POST endpoint to routes/projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const params = require('params');
 
-const Project = require('../models/project.js');
+const Project = require('../models/project');
 
 // GET all projects
 router.get('/', (req, res, next) => {
@@ -35,6 +36,32 @@ router.get('/:id', (req, res, next) => {
       console.log('Error message: ', err);
       res.sendStatus(500);
     });
+});
+
+// POST create new project
+router.post('/create', (req, res, next) => {
+  const clients_ids = req.body.clients_ids;
+  const collaborators_ids = req.body.collaborators_ids;
+
+  const allowedKeys = ['name', 'date', 'description'];
+  const formData = params(req.body).only(allowedKeys);
+
+  if (Object.keys(formData).length != 0) {
+    Project
+      .forge(formData)
+      .save()
+      .then((project) => {
+        project.clients().attach(clients_ids);
+        project.collaborators().attach(collaborators_ids);
+        res.redirect(`/projects/${project.id}`);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+    } else {
+      res.status(400).send('Bad Request');
+    };
 });
 
 module.exports = router;

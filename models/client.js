fc6a39f07b2a2d392e3f93ae0717f0cb6ca1476b
wedesignstudios <deelden@gmail.com>
2017-03-21 12:24:56 -0500

    edit Client model dependents

--- models/client.js
+++ models/client.js
@@ -14,7 +14,7 @@ const Client = bookshelf.Model.extend({
     return this.hasOne('Address');
   }
 }, {
-  dependents: ['projects', 'address']
+  dependents: ['projects']
 });
 
 module.exports = bookshelf.model('Client', Client);

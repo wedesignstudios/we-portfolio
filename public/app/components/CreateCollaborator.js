abb215b4a435bbdef4e4fb9a472c882adf4d21ba
wedesignstudios <deelden@gmail.com>
2017-04-12 16:26:30 -0500

    Add <div> elements to CreateCollaborator form component

--- public/app/components/CreateCollaborator.js
+++ public/app/components/CreateCollaborator.js
@@ -7,11 +7,17 @@ class CreateCollaborator extends React.Component {
       <div>
         <h3>Add A New Collaborator</h3>
         <form>
-          <label>Collaborator Name: </label>
-          <input type="text" name="name" />
-          <label>Website: </label>
-          <input type="text" name="url" />
-          <button type="submit">Submit</button>
+          <div>
+            <label>Collaborator Name: </label>
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <label>Website: </label>
+            <input type="text" name="url" />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
         </form>
       </div>
     );

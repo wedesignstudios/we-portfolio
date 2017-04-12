d2f65c76e28767120672f4a15392bd5b44349496
wedesignstudios <deelden@gmail.com>
2017-04-12 16:25:32 -0500

    Add <div> elements to CreateClient form component

--- public/app/components/CreateClient.js
+++ public/app/components/CreateClient.js
@@ -7,11 +7,17 @@ class CreateClient extends React.Component {
       <div>
         <h3>Add A New Client</h3>
         <form>
-          <label>Client Name: </label>
-          <input type="text" name="name" />
-          <label>Website: </label>
-          <input type="text" name="url" />
-          <button type="submit">Submit</button>
+          <div>
+            <label>Client Name: </label>
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <label>Website: </label>
+            <input type="text" name="url" />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
         </form>
       </div>
     );

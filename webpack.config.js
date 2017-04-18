99f3246d147a556297b00beb68a331ef7e8fe5f6
wedesignstudios <deelden@gmail.com>
2017-04-18 16:31:19 -0500

    add css loader to webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var webpack = require('webpack')
-var path = require('path')
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -19,7 +20,17 @@ module.exports = {
        query:{
            presets: ['react','es2015']
        }
+     },
+     {
+       test: /\.css$/,
+       loader: ExtractTextPlugin.extract({
+         fallback: "style-loader",
+         use: "css-loader"
+       })
      }
    ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("./public/stylesheets/component_styles.css"),
+  ]
 }﻿

cf9dfe7000bfbfef034034addd23d13869971264
wedesignstudios <deelden@gmail.com>
2017-04-21 10:39:11 -0500

    create DataActions library

---
+++ public/app/data/actions.js
@@ -0,0 +1,22 @@
+const DataActions = {};
+
+DataActions.postRequest = (state, apiEndpoint) => {
+  const data = state;
+
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("POST", apiEndpoint);
+  xhr.setRequestHeader("content-type", "application/json; charset=UTF-8");
+  xhr.setRequestHeader("cache-control", "no-cache");
+
+  xhr.send(JSON.stringify(data));
+}
+
+module.exports = DataActions;

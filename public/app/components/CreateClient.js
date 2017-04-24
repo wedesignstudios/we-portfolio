a2ce5e01bc8d56fd4d6911c33c52593cca18858e
wedesignstudios <deelden@gmail.com>
2017-04-24 10:26:21 -0500

    add state object, error messages to CreateClient component

--- public/app/components/CreateClient.js
+++ public/app/components/CreateClient.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 class CreateClient extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      url: '',
+      nameErr: false,
+      urlErr: false
+    }
+    
+  }
+
   render() {
     return (
       <div>
@@ -10,10 +22,12 @@ class CreateClient extends React.Component {
           <div>
             <label>Client Name: </label>
             <input type="text" name="name" />
+            {this.state.nameErr ? <div id="client-name-validation-error" style={{color: 'red'}}>Name can not be blank. Please enter a project name.</div> : null}
           </div>
           <div>
             <label>Website: </label>
             <input type="text" name="url" />
+            {this.state.urlErr ? <div id="client-url-validation-error" style={{color: 'red'}}>Website can not be blank. Please enter a project name.</div> : null}
           </div>
           <div>
             <button type="submit">Submit</button>

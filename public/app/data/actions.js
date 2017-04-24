bd9cbe8614be369e63f805f38a929dd3653085ff
wedesignstudios <deelden@gmail.com>
2017-04-24 10:23:16 -0500

    refactor DataActions object

--- public/app/data/actions.js
+++ public/app/data/actions.js
@@ -1,22 +1,25 @@
-const DataActions = {};
+const DataActions = {
 
-DataActions.postRequest = (state, apiEndpoint) => {
-  const data = state;
+  postRequest: (state, apiEndpoint) => {
+    const data = state;
 
-  const xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  });
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
 
-  xhr.open("POST", apiEndpoint);
-  xhr.setRequestHeader("content-type", "application/json; charset=UTF-8");
-  xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.open("POST", apiEndpoint);
+    xhr.setRequestHeader("content-type", "application/json; charset=UTF-8");
+    xhr.setRequestHeader("cache-control", "no-cache");
 
-  xhr.send(JSON.stringify(data));
-}
+    xhr.send(JSON.stringify(data));
+  }
+
+};
+
 
 module.exports = DataActions;

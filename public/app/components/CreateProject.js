6127507415faff8fa126215c8a9daeff4f2e159e
wedesignstudios <deelden@gmail.com>
2017-04-24 10:29:44 -0500

    require FormHandlers service, refactor to implement service

--- public/app/components/CreateProject.js
+++ public/app/components/CreateProject.js
@@ -6,6 +6,7 @@ import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const DataActions = require('../data/actions');
+const FormHandlers = require('../services/form_input_handlers');
 
 class CreateProject extends React.Component {
   constructor() {
@@ -21,27 +22,10 @@ class CreateProject extends React.Component {
       dateClear: false,
     }
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleDateInputChange = this.handleDateInputChange.bind(this);
+    this.handleInputChange = FormHandlers.inputChange;
+    this.handleDateInputChange = FormHandlers.dateInputChange;
     this.validateForm = this.validateForm.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleDateInputChange(date) {
-    this.setState({
-      date: date,
-      dateClear: true
-    });
-  }
+  }  
 
   validateForm(event) {
     const {name, date, description} = this.state;
@@ -71,8 +55,8 @@ class CreateProject extends React.Component {
         <form>
           <div>
             <label>Project Name: </label>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
-            {this.state.nameErr ? <div id="name-validation-error" style={{color: 'red'}}>Name can not be blank. Please enter a project name.</div> : null}
+            <input type="text" name="name" value={this.state.name} onChange={(e) => this.handleInputChange(e)} />
+            {this.state.nameErr ? <div id="project-name-validation-error" style={{color: 'red'}}>Name can not be blank. Please enter a project name.</div> : null}
           </div>
 
           <div>
@@ -86,14 +70,14 @@ class CreateProject extends React.Component {
                 popoverTargetAttachment="top center"
                 popoverTargetOffset="30px 0px"
                 isClearable={this.state.dateClear}
-                onChange={this.handleDateInputChange} />
-            {this.state.dateErr ? <div id="date-validation-error" style={{color: 'red'}}>Date can not be blank. Please enter a project completed date.</div> : null}
+                onChange={(e) => this.handleDateInputChange(e)} />
+            {this.state.dateErr ? <div id="project-date-validation-error" style={{color: 'red'}}>Date can not be blank. Please enter a project completed date.</div> : null}
           </div>
 
           <div>
             <label>Description: </label>
-            <input type="textfield" name="description" value={this.state.description} onChange={this.handleInputChange} />
-            {this.state.descriptionErr ? <div id="description-validation-error" style={{color: 'red'}}>Description can not be blank. Please enter a project description.</div> : null}
+            <input type="textfield" name="description" value={this.state.description} onChange={(e) => this.handleInputChange(e)} />
+            {this.state.descriptionErr ? <div id="project-description-validation-error" style={{color: 'red'}}>Description can not be blank. Please enter a project description.</div> : null}
           </div>
           <div>
             <button onClick={this.validateForm}>Submit</button>

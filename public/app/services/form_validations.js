8fc80538d072993f7765f88d0fb82809b61609da
wedesignstudios <deelden@gmail.com>
2017-04-28 13:02:32 -0500

    add form validation functions

--- public/app/services/form_validations.js
+++ public/app/services/form_validations.js
@@ -1,1 +1,58 @@
-// Form Validations here
+const FormValidations = {
+
+  isFieldBlank: function(event) {
+    const target = event.target;
+    const value = target.value;
+
+    return !value;
+  },
+
+  isMomentObjectNull: function(event) {
+    const dateMoment = event;
+
+    return dateMoment === null;
+  },
+
+  areAnyRequiredFieldsBlank: function(_this, nextState) {
+    let reqFields = _this.requiredFields;
+
+    for(let i=0; i<reqFields.length; i++) {
+      if(nextState[reqFields[i]] === '') {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  checkRequiredField: function(event) {
+    const target = event.target;
+    const value = target.value;
+    const fieldName = target.name;
+    const err = event.target.name + 'Err';
+
+    if(FormValidations.isFieldBlank(event)) {
+      this.setState({[err]: true});
+    } else {
+      this.setState({[err]: false});
+    }
+  },
+
+  checkForBlanks: function() {
+    const argArr = arguments[0];
+
+    for(let i=0; i<argArr.length; i++) {
+      var arg = argArr[i];
+      var argStateValue = this.state[arg];
+      var err = `${arg}Err`;
+
+      if (!argStateValue) {
+        this.setState({[err]: true});
+      } else {
+        this.setState({[err]: false});
+      }
+    }
+  }
+
+}
+
+module.exports = FormValidations;

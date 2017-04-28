12c69734f90c7ee38ce828ffefba4dda82b775a1
wedesignstudios <deelden@gmail.com>
2017-04-28 12:59:30 -0500

    add handlers: preventAllButShiftAndTab, handleOnChange; include _this arg in functions

--- public/app/services/form_input_handlers.js
+++ public/app/services/form_input_handlers.js
@@ -1,19 +1,45 @@
+const FormValidations = require('./form_validations');
+
 const FormHandlers = {
 
-  inputChange: function(event) {
+  handleOnChange: function(event, _this) {
+    if(event._isAMomentObject) {
+      FormHandlers.dateInputChange(event, _this);
+    } else {
+      FormHandlers.inputChange(event, _this);
+    }
+  },
+
+  inputChange: function(event, _this) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
 
-    this.setState({
+    _this.setState({
       [name]: value
     });
   },
 
-  dateInputChange: function(event) {
-    this.setState({
+  dateInputChange: function(event, _this) {
+    _this.setState({
       date: event,
       dateClear: true
+    });
+
+    if(FormValidations.isMomentObjectNull(event)) {
+      _this.setState({dateErr: true});
+    } else {
+      _this.setState({dateErr: false});
+    }
+  },
+
+  preventAllButShiftAndTab: function(event) {
+    event.target.addEventListener('keydown', function(event) {
+      let key = event.key;
+
+      if(key !== 'Shift' && key !== 'Tab') {
+        event.preventDefault();
+      };
     });
   }
 

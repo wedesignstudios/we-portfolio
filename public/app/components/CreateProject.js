fc6fb02cc0359dd6a75cb18b196d01f46a507c3f
wedesignstudios <deelden@gmail.com>
2017-04-28 12:56:53 -0500

    add/refactor validations, event handlers; diable submit until valid; add shouldComponentUpdate() to check state

--- public/app/components/CreateProject.js
+++ public/app/components/CreateProject.js
@@ -7,6 +7,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 const DataActions = require('../data/actions');
 const FormHandlers = require('../services/form_input_handlers');
+const FormValidations = require('../services/form_validations');
 
 class CreateProject extends React.Component {
   constructor() {
@@ -18,34 +19,35 @@ class CreateProject extends React.Component {
       description: '',
       nameErr: false,
       dateErr: false,
-      descriptionErr: false,
-      dateClear: false,
+      descriptionErr: false
     }
 
-    this.handleInputChange = FormHandlers.inputChange;
-    this.handleDateInputChange = FormHandlers.dateInputChange;
+    this.requiredFields = ['name', 'date', 'description'];
+    this.requiredFieldsBlank = true;
+    this.handleOnChange = FormHandlers.handleOnChange;
+    this.validateCheckRequiredField = FormValidations.checkRequiredField;
+    this.handleKeyPress = FormHandlers.preventAllButShiftAndTab;
+
+    // Marked for removal:
     this.validateForm = this.validateForm.bind(this);
-  }  
+    this.validateFormCheckForBlanks = FormValidations.checkForBlanks;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if(FormValidations.areAnyRequiredFieldsBlank(this, nextState)) {
+      this.requiredFieldsBlank = true;
+    } else {
+      this.requiredFieldsBlank = false;
+    };
+
+    return true;
+  }
 
   validateForm(event) {
-    const {name, date, description} = this.state;
     event.preventDefault();
+    this.validateFormCheckForBlanks(this.requiredFields);
 
-    if (!name || !date || !description) {
-      !name ? this.setState({nameErr: true}) : this.setState({nameErr: false});
-      !date ? this.setState({dateErr: true}) : this.setState({dateErr: false});
-      !description ? this.setState({descriptionErr: true}) : this.setState({descriptionErr: false});
-
-      return;
-    } else {
-      this.setState({
-        nameErr: false,
-        dateErr: false,
-        descriptionErr: false
-      });
-    }
-
-    DataActions.postRequest(this.state, '/projects');
+    // DataActions.postRequest(this.state, '/projects');
   }
 
   render() {
@@ -55,32 +57,46 @@ class CreateProject extends React.Component {
         <form>
           <div>
             <label>Project Name: </label>
-            <input type="text" name="name" value={this.state.name} onChange={(e) => this.handleInputChange(e)} />
+            <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={(e) => this.handleOnChange(e, this)}
+                onBlur={(e) => this.validateCheckRequiredField(e)} />
             {this.state.nameErr ? <div id="project-name-validation-error" style={{color: 'red'}}>Name can not be blank. Please enter a project name.</div> : null}
           </div>
 
           <div>
             <label>Date Completed: </label>
-            <DatePicker 
+            <DatePicker
                 selected={this.state.date}
                 value={this.state.date}
-                name="date2"
+                name="date"
+                showMonthDropdown
+                showYearDropdown
+                dropdownMode="select"
                 placeholderText="Click to select a date"
                 popoverAttachment="top right"
                 popoverTargetAttachment="top center"
                 popoverTargetOffset="30px 0px"
-                isClearable={this.state.dateClear}
-                onChange={(e) => this.handleDateInputChange(e)} />
+                onChange={(e) => this.handleOnChange(e, this)}
+                onFocus={(e) => this.handleKeyPress(e)}
+                onBlur={(e) => this.validateCheckRequiredField(e)} />
             {this.state.dateErr ? <div id="project-date-validation-error" style={{color: 'red'}}>Date can not be blank. Please enter a project completed date.</div> : null}
           </div>
 
           <div>
             <label>Description: </label>
-            <input type="textfield" name="description" value={this.state.description} onChange={(e) => this.handleInputChange(e)} />
+            <input
+                type="textfield"
+                name="description"
+                value={this.state.description}
+                onChange={(e) => this.handleOnChange(e, this)}
+                onBlur={(e) => this.validateCheckRequiredField(e)} />
             {this.state.descriptionErr ? <div id="project-description-validation-error" style={{color: 'red'}}>Description can not be blank. Please enter a project description.</div> : null}
           </div>
           <div>
-            <button onClick={this.validateForm}>Submit</button>
+            <button disabled={this.requiredFieldsBlank} onClick={this.validateForm}>Submit</button>
           </div>
         </form>
       </div>

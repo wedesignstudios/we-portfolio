a3d84e423aadcd2c28915e9c8b37c921f28e264b
wedesignstudios <deelden@gmail.com>
2017-05-11 16:10:23 -0500

    add ';' after import statements

--- public/app/components/CreateImage.js
+++ public/app/components/CreateImage.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class CreateImage extends React.Component {
   render() {

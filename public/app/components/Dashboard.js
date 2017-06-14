808ca3a603c0175d3b0bf07d9292e5dc8ed2021e
wedesignstudios <deelden@gmail.com>
2017-06-14 12:17:18 -0500

    add DashboardHome route, rename UpdateProject routes in Dashboard component

--- public/app/components/Dashboard.js
+++ public/app/components/Dashboard.js
@@ -9,12 +9,14 @@ import {
   withRouter
 } from 'react-router-dom';
 
+const DashboardHome = require('./DashboardHome');
 const CreateProject = require('./CreateProject');
 const UpdateProject = require('./UpdateProject');
 const CreateClient = require('./CreateClient');
 const CreateCollaborator = require('./CreateCollaborator');
 const UploadImages = require('./UploadImages');
 const GetImages = require('./GetImages');
+const GetProjects = require('./GetProjects');
 const NotFound = require('./NotFound');
 
 class Dashboard extends Component {
@@ -23,13 +25,16 @@ class Dashboard extends Component {
     return (
       <Router>
       <div>
+        <h1>WE Portfolio Dashboard</h1>
         <Switch>
+          <Route exact path={`${this.props.match.url}`} component={DashboardHome} />
           <Route path={`${this.props.match.url}/create-project`} component={CreateProject} />
           <Route path={`${this.props.match.url}/create-client`} component={CreateClient} />
           <Route path={`${this.props.match.url}/create-collaborator`} component={CreateCollaborator} />
           <Route path={`${this.props.match.url}/upload-images`} component={UploadImages} />
           <Route path={`${this.props.match.url}/update-images`} component={GetImages} />
-          <Route path={`${this.props.match.url}/update-project/:id`} component={UpdateProject} />
+          <Route path={`${this.props.match.url}/update-projects/:id`} component={UpdateProject} />
+          <Route path={`${this.props.match.url}/update-projects`} component={GetProjects} />
           <Route component={NotFound} />
         </Switch>
       </div>

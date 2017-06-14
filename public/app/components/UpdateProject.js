38459dd0676fa3dbd2ffa147def48d378daf1f34
wedesignstudios <deelden@gmail.com>
2017-06-14 10:11:04 -0500

    remove redundant code, pass in params.id to projectId prop

--- public/app/components/UpdateProject.js
+++ public/app/components/UpdateProject.js
@@ -4,33 +4,10 @@ import ReactDOM from 'react-dom';
 const FormProject = require('./FormProject');
 
 class UpdateProject extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      name: ''      
-    }
-    
-  }
-
-  componentDidMount() {
-    fetch(`/api/projects/${this.props.projectId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({
-          name: data.name          
-        })
-      })
-      .catch((err) => {
-        console.error(err);
-      });      
-  }
-
   render() {
     return (
       <div>
-        <h3>CREATE/UPDATE PROJECT: {this.state.name}</h3>
-        <FormProject sendRequestType="PUT" projectId={this.props.projectId} />
+        <FormProject sendRequestType="PUT" projectId={this.props.match.params.id} />
       </div>
     );
   }

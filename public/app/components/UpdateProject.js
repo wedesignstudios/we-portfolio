3fa46ff1b506df5ebd3dd3c77b9659be96fb5a17
wedesignstudios <deelden@gmail.com>
2017-06-14 18:32:42 -0500

    add Back link on UpdateProject

--- public/app/components/UpdateProject.js
+++ public/app/components/UpdateProject.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
 const FormProject = require('./FormProject');
 
@@ -7,6 +8,7 @@ class UpdateProject extends React.Component {
   render() {
     return (
       <div>
+        <Link to='/dashboard/update-projects'>Back</Link>
         <FormProject sendRequestType="PUT" projectId={this.props.match.params.id} />
       </div>
     );

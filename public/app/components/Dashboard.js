67402a7ed91df34cc93095351bd16663e37da5e6
wedesignstudios <deelden@gmail.com>
2017-06-14 10:08:58 -0500

    refactor Dashboard to use router switch

--- public/app/components/Dashboard.js
+++ public/app/components/Dashboard.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
 
 const CreateProject = require('./CreateProject');
 const UpdateProject = require('./UpdateProject');
@@ -7,20 +15,25 @@ const CreateClient = require('./CreateClient');
 const CreateCollaborator = require('./CreateCollaborator');
 const UploadImages = require('./UploadImages');
 const GetImages = require('./GetImages');
+const NotFound = require('./NotFound');
 
 class Dashboard extends Component {
 
   render() {
     return (
+      <Router>
       <div>
-        <a href='/logout'>Logout</a>
-        <UpdateProject projectId='79' />
-        <CreateProject />
-        <CreateClient />
-        <CreateCollaborator />       
-        <UploadImages />
-        <GetImages />
+        <Switch>
+          <Route path={`${this.props.match.url}/create-project`} component={CreateProject} />
+          <Route path={`${this.props.match.url}/create-client`} component={CreateClient} />
+          <Route path={`${this.props.match.url}/create-collaborator`} component={CreateCollaborator} />
+          <Route path={`${this.props.match.url}/upload-images`} component={UploadImages} />
+          <Route path={`${this.props.match.url}/update-images`} component={GetImages} />
+          <Route path={`${this.props.match.url}/update-project/:id`} component={UpdateProject} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
+      </Router>
     );
   }
 }

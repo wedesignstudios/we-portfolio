9c806b611725c85f2152f07bff05a349b60aa203
wedesignstudios <deelden@gmail.com>
2017-06-14 10:12:24 -0500

    remove header from CreateProject component

--- public/app/components/CreateProject.js
+++ public/app/components/CreateProject.js
@@ -4,11 +4,9 @@ import ReactDOM from 'react-dom';
 const FormProject = require('./FormProject');
 
 class CreateProject extends React.Component {
-
   render() {
     return (
       <div>
-        <h3>Add A New Project</h3>
         <FormProject sendRequestType="POST" />
       </div>
     );

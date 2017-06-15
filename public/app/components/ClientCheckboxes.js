b9177b31106e901631f8cc4091a55e35d211fd7e
wedesignstudios <deelden@gmail.com>
2017-06-15 10:18:56 -0500

    remove checkboxChange handler, add FormHandler multiCheckboxChange handler

--- public/app/components/ClientCheckboxes.js
+++ public/app/components/ClientCheckboxes.js
@@ -25,38 +25,6 @@ class ClientCheckboxes extends React.Component {
       });    
   }
 
-  checkboxChange(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = parseInt(target.value);
-    var checked = this.props.preChecked;
-    var attached = this.props.attached;
-    var detach = this.props.detach;
-    var toAttach = this.props.toAttach;
-
-    if(attached.includes(value)) {
-      let index = detach.indexOf(value);
-      detach.includes(value) ? detach.splice(index, 1) : detach.push(value);      
-    } else {
-      let index = toAttach.indexOf(value);      
-      toAttach.includes(value) ? toAttach.splice(index, 1) : toAttach.push(value);
-    }
-
-    if(checked.includes(value)) {
-      let index = checked.indexOf(value);
-      checked.splice(index, 1);
-    } else {
-      checked.push(value);
-    }
-
-    this.props.sendClientData({
-      checked: checked,
-      attached: attached,
-      toAttach: toAttach,
-      detach: detach
-    });    
-  }
-
   render() {
     const { preChecked, sendClientData, attached, detach } = this.props;
 
@@ -71,7 +39,7 @@ class ClientCheckboxes extends React.Component {
                 value={client.id}
                 name="clients_ids"
                 checked={this.props.preChecked.includes(client.id)}
-                onChange={(e) => this.checkboxChange(e)} />
+                onChange={(e) => FormHandlers.multiCheckboxChange(e, this, this.props.sendClientData)} />
               <label>{client.name}</label>
             </div>)}
         </div>        

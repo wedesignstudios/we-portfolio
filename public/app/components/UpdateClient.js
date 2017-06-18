5bc9711a358cfc6ea98c4ac5133540e27261b8b4
wedesignstudios <deelden@gmail.com>
2017-06-18 13:13:31 -0500

    add UpdateClient component

---
+++ public/app/components/UpdateClient.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+
+const FormClient = require('./FormClient');
+
+class UpdateClient extends Component {
+  render() {
+    return(
+      <div>
+        <Link to='/dashboard/update-clients'>All Clients</Link>
+        <FormClient sendRequestType="PUT" clientId={this.props.match.params.id} />        
+      </div>
+    );
+  }
+}
+
+module.exports = UpdateClient;

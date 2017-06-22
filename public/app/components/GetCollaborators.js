5f19daf4d255a5bfa19a781ab47ed37ef07d3bdb
wedesignstudios <deelden@gmail.com>
2017-06-22 17:12:00 -0500

    add GetCollaborators component

---
+++ public/app/components/GetCollaborators.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+
+class GetCollaborators extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      collaboratorsData: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/collaborators')
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          collaboratorsData: data
+        })
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    return(
+      <div>
+        <h3>All Collaborators</h3>
+          <div>
+            {this.state.collaboratorsData.map(collaborator =>
+              <div key={collaborator.id}><Link to={`${this.props.match.url}/${collaborator.id}`}>{collaborator.name}</Link></div>
+            )}
+          </div>
+      </div>
+    );
+  }
+}
+
+module.exports = GetCollaborators;

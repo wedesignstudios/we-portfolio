700de669f7e448ff3ee6cc9e4f12469b5f050703
wedesignstudios <deelden@gmail.com>
2017-06-22 12:04:06 -0500

    remove space from migration file

--- migrations/20170618085445_reconfigure_addresses_clients_collaborators_tables.js
+++ migrations/20170618085445_reconfigure_addresses_clients_collaborators_tables.js
@@ -2,27 +2,21 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTableIfNotExists('addresses_clients', (tbl) => {
       tbl.increments('id').primary();
-      tbl.integer('address_id');
-      tbl.integer('client_id');
+      tbl.integer('address_id').references('addresses.id');
+      tbl.integer('client_id').references('clients.id');
       tbl.timestamps();
     }),
     knex.schema.createTableIfNotExists('addresses_collaborators', (tbl) => {
       tbl.increments('id').primary();
-      tbl.integer('address_id');
-      tbl.integer('collaborator_id');
+      tbl.integer('address_id').references('addresses.id');
+      tbl.integer('collaborator_id').references('collaborators.id');
       tbl.timestamps();
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.table('clients', (tbl) => {
-      tbl.dropColumn('address_id');
-    }),
-    knex.schema.table('collaborators', (tbl) => {
-      tbl.dropColumn('address_id');
-    }),
+  return Promise.all([    
     knex.schema.dropTableIfExists('addresses_clients'),
     knex.schema.dropTableIfExists('addresses_collaborators')
   ]);

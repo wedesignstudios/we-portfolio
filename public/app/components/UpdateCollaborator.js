5ad6422f3e256cb46f22a77de17ade84c3bdd57f
wedesignstudios <deelden@gmail.com>
2017-06-22 17:25:02 -0500

    code out UpdateCollaborator component

--- public/app/components/UpdateCollaborator.js
+++ public/app/components/UpdateCollaborator.js
@@ -1,1 +1,18 @@
-UpdateCollaborator.js
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+
+const FormCollaborator = require('./FormCollaborator');
+
+class UpdateCollaborator extends Component {
+  render() {
+    return(
+      <div>
+        <Link to='/dashboard/update-collaborators'>All Collaborators</Link><br />
+        <FormCollaborator sendRequestType="PUT" collaboratorId={this.props.match.params.id} />
+      </div>
+    );
+  }
+}
+
+module.exports = UpdateCollaborator;

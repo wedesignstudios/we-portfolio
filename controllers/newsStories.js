f3d183e70297d5cd321621f7338ee22ba8f1c379
wedesignstudios <deelden@gmail.com>
2017-06-28 12:42:24 -0500

    add NewsStories controller SHOW/CREATE routes

--- controllers/newsStories.js
+++ controllers/newsStories.js
@@ -23,7 +23,48 @@ router.get('/', (req, res, next) => {
     });
 });
 
+// SHOW a news story by ID
+router.get('/:id', (req, res, next) => {
+  NewsStory
+    .forge({id: req.params.id})
+    .fetch({
+      withRelated: ['image', 'news_categories'],
+      debug: true
+    })
+    .then((story) => {
+      if(story) {
+        res.json(story);
+      } else {
+        res.json(null);
+      }
+    })
+    .catch((err) => {
+      console.error('Error message: ', err);
+      res.sendStatus(500);
+    });
+});
+
 // CREATE new news story
+router.post('/', isLoggedIn, (req, res, next) => {
+  const allowedKeys = ['title', 'date', 'description'];
+  const formData = params(req.body).only(allowedKeys);
+
+  if(Object.keys(formData).length != 0) {
+    NewsStory
+      .forge(formData)
+      .save()
+      .then((story) => {
+        story = story.toJSON();
+        return res.status(200).send(`${story.title} successfully created.`);
+      })
+      .catch((err) => {
+        console.error('Error message: ', err);
+        res.send(500).send(`Whoops! The following error occurred: ${err}`);
+      });
+    } else {
+      res.status(400).send('Bad Request');
+    };
+});
 
 // UPDATE news story
 

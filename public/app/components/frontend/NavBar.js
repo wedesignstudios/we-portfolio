567d0d37f01adaa8fe230d8ff73ad7f6889cefec
wedesignstudios <deelden@gmail.com>
2017-07-10 12:08:00 -0500

    add we eye logo to navbar

--- public/app/components/frontend/NavBar.js
+++ public/app/components/frontend/NavBar.js
@@ -5,10 +5,11 @@ import { Link } from 'react-router-dom';
 class NavBar extends Component {
   render() {
     return (
-      <div>
+      <div id="navbar-main">
         <ul>
           <li><Link to="/press">Press</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><img src="https://we-portfolio.s3.amazonaws.com/we-eye-logo-white.svg" /></li>
           <li><Link to="/work">Work</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>

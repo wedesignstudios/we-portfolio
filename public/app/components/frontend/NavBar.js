4486fc0fd93030c4f9652c4aae87a5fbb8761adb
wedesignstudios <deelden@gmail.com>
2017-07-10 14:45:41 -0500

    add NavLink, activeClassName to NavBar to set active

--- public/app/components/frontend/NavBar.js
+++ public/app/components/frontend/NavBar.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
+import {
+  Link,
+  NavLink
+} from 'react-router-dom';
 
 class NavBar extends Component {
   render() {
     return (
-      <div id="navbar-main">
-        <ul>
-          <li><Link to="/press">Press</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><img src="https://we-portfolio.s3.amazonaws.com/we-eye-logo-white.svg" /></li>
-          <li><Link to="/work">Work</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
+      <div id="navbar-main" className="row justify-content-center">
+        <div className="col-9">
+          <ul className="nav nav-fill align-items-center justify-content-center">
+            <li className="nav-item">
+              <NavLink to="/work" className="nav-link" activeClassName="nav-active">Work</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link" activeClassName="nav-active">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <Link to="/"><img className="nav-link mx-auto"src="https://we-portfolio.s3.amazonaws.com/we-eye-logo-white.svg" /></Link>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/press" className="nav-link" activeClassName="nav-active">Press</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/contact" className="nav-link" activeClassName="nav-active">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }

611d81d511c3c23160d6c3b4e04f731b2e5a2cb4
wedesignstudios <deelden@gmail.com>
2017-07-19 10:38:22 -0500

    add styles, initialName to state

--- public/app/components/dashboard/FormClient.js
+++ public/app/components/dashboard/FormClient.js
@@ -17,6 +17,7 @@ class FormClient extends React.Component {
 
     this.state = {
       name: '',
+      initialName: '',
       url: '',
       city: '',
       state: '',
@@ -43,6 +44,7 @@ class FormClient extends React.Component {
         .then((data) => {
           this.setState({
             name: data.name,
+            initialName: data.name,
             url: data.url,
           });
           if(data.address.length > 0) {
@@ -114,52 +116,104 @@ class FormClient extends React.Component {
 
   render() {
     return (
-      <div>
-        <Link to='/dashboard/clients'>All Clients</Link><br />
-        <h3>{this.props.sendRequestType === 'POST' ? 'Create A New Client' : `Update Client: ${this.state.name}`}</h3>
-        <div className="submit-message-success">
-          {this.state.submitSuccess ? <div id="client-added-success" style={{color: 'green'}}><p>{this.props.sendRequestType === 'POST' ? 'New Client successfully added.' : 'Client successfully updated.'}</p></div> : null}
-        </div>
-        <div className="submit-message-error" style={{color: 'red'}}><p>{this.state.submitError}</p></div>
-        {this.props.sendRequestType === 'PUT' ?
-          <button onClick={(e) => this.deleteClient(e)}>Delete {this.state.name}</button> :
-        null}
-        <form id="create-client">
-          <div>
-            <label>Client Name: </label>
-            <input
-                type="text"
-                name="name"
-                className={this.state.nameErr ? 'err' : null}
-                value={this.state.name}
-                onChange={(e) => FormHandlers.handleOnChange(e, this)}
-                onBlur={(e) => FormValidations.checkField(e, this)} />
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <Link to='/dashboard/clients' className="btn btn-primary mb-3">All Clients</Link>
+          <h1>
+            <span className="badge badge-default p-3">
+            {this.props.sendRequestType === 'POST' ? 'Create A New Client' : `Update Client: ${this.state.initialName}`}
+            </span>
+          </h1>
+
+          {this.props.sendRequestType === 'PUT' ?
+            <div className="d-flex justify-content-end pr-3">
+              <button
+                className="btn btn-danger mb-3"
+                onClick={(e) => this.deleteClient(e)}>
+                  Delete {this.state.initialName}
+              </button>
+            </div> :
+          null}
+
+          <div className="submit-message-error">
+            {this.state.submitError ?
+              <div className="alert alert-danger">
+                {this.state.submitError}
+              </div>
+            : null}
           </div>
-          <div>
-            <label>Website: </label>
-            <input
-                type="text"
-                name="url"
-                className={this.state.urlErr ? 'err' : null}
-                value={this.state.url}
-                onChange={(e) => FormHandlers.handleOnChange(e, this)}
-                onBlur={(e) => FormValidations.checkField(e, this)} />
+
+          <div className="errors row">
+            <div className="col-sm-10">
+              {this.state.nameErr ?
+                <div
+                  id="client-name-validation-error"
+                  className="alert alert-danger">
+                    Client Name can not be blank. Please enter a Client Name.
+                </div> :
+              null}
+              {(this.state.urlErr && this.state.urlErrType === 'not valid') ?
+                <div
+                  id="client-url-validation-error"
+                  className="alert alert-danger">
+                    Website URL is not valid. Please enter a valid Website URL.
+                </div> :
+              null}
+            </div>
           </div>
-          <div>
-            <label>Address: </label>
-            <FormAddress
-              clientId={this.props.clientId}
-              sendAddressData={this.getComponentData} />
+
+          <div className="container-fluid">
+            <form id="create-client">
+              <div className="form-group row">
+                <label className="col-sm-2 col-form-label">Client Name: </label>
+                  <div className="col-sm-10">
+                    <input
+                        type="text"
+                        name="name"
+                        className={this.state.nameErr ? 'err form-control' : 'form-control'}
+                        value={this.state.name}
+                        onChange={(e) => FormHandlers.handleOnChange(e, this)}
+                        onBlur={(e) => FormValidations.checkField(e, this)} />
+                  </div>
+              </div>
+
+              <div className="form-group row">
+                <label className="col-sm-2 col-form-label">Website: </label>
+                <div className="col-sm-10">
+                  <input
+                      type="text"
+                      name="url"
+                      className={this.state.urlErr ? 'err form-control' : 'form-control'}
+                      value={this.state.url}
+                      onChange={(e) => FormHandlers.handleOnChange(e, this)}
+                      onBlur={(e) => FormValidations.checkField(e, this)} />
+                </div>
+              </div>
+
+              <div className="form-group row">
+                <label className="col-sm-2 col-form-label">Address: </label>
+                <div className="col-sm-10">
+                  <FormAddress
+                    clientId={this.props.clientId}
+                    sendAddressData={this.getComponentData} />
+                </div>
+              </div>
+
+              <div className="form-group row">
+                <div className="col-sm-12 d-flex justify-content-end">
+                  <Link to='/dashboard/clients' className="btn btn-secondary mr-3">Cancel</Link><br />
+                  <button
+                    className="btn btn-primary"
+                    disabled={this.requiredFieldsBlank}
+                    onClick={(e) => this.submitForm(e)}>
+                      {this.props.sendRequestType === 'PUT' ?
+                        `Update ${this.state.initialName}` :
+                        'Submit'}
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div>
-            <button disabled={this.requiredFieldsBlank} onClick={(e) => this.submitForm(e)}>
-              {this.props.sendRequestType === 'POST' ? 'Submit' : `Update ${this.state.name}`}
-            </button>
-          </div>
-        </form>
-        <div className="errors">
-          {this.state.nameErr ? <div id="client-name-validation-error" style={{color: 'red'}}>Client Name can not be blank. Please enter a Client Name.</div> : null}
-          {(this.state.urlErr && this.state.urlErrType === 'not valid') ? <div id="client-url-validation-error" style={{color: 'red'}}>Website URL is not valid. Please enter a valid Website URL.</div> : null}
         </div>
       </div>
     );

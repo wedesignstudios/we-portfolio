ba43ec1e566035e76b490cf151c02a86303b9b4a
wedesignstudios <deelden@gmail.com>
2017-08-03 17:36:03 -0500

    add project_images_order table migration

---
+++ migrations/20170803172807_add_project_images_order_table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('project_images_order', (tbl) => {
+      tbl.increments('id').primary();
+      tbl.integer('project_id').references('projects.id');
+      tbl.specificType('images_order', 'integer[]');
+      tbl.timestamps();
+    })
+  ])  
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTableIfExists('project_images_order')
+  ])
+};

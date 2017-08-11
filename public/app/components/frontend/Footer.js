843c01df951814655a4f6d71e57e68bcb8a3a6af
wedesignstudios <deelden@gmail.com>
2017-08-11 12:39:17 -0500

    remove footer padding

--- public/app/components/frontend/Footer.js
+++ public/app/components/frontend/Footer.js
@@ -11,8 +11,8 @@ class Footer extends Component {
 
   render() {
     return (
-      <div id="footer" className="row justify-content-center">
-        <div className="col">
+      <div id="footer" className="row justify-content-center m-0">
+        <div className="col p-0">
           <div className="d-flex justify-content-center">&copy;2010-{this.today.getFullYear()} WE Design Studios LLC</div>
         </div>
       </div>

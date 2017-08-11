6c378105f65a685bbc6400b1449d251ff18205ce
wedesignstudios <deelden@gmail.com>
2017-08-11 12:40:23 -0500

    add image portrait orientation, add img-landscape class

--- public/app/components/frontend/Index.js
+++ public/app/components/frontend/Index.js
@@ -6,10 +6,8 @@ class Index extends Component {
     return (
       <div className="row justify-content-center m-0 image-full-width">
         <div className="col p-0 image-full-width-container">
-          <img className="img-fluid" src="https://we-portfolio.s3.amazonaws.com/LAURA_WEST_TX.jpg" alt="Laura out in West Texas" />
-        </div>
-        <div className="col p-0 image-full-width-text">
-          <h5 className="muli-bold d-flex justify-content-center">WE DESIGN STUDIOS</h5>
+          <img className="img-fluid img-landscape" src="https://we-portfolio.s3.amazonaws.com/LAURA_WEST_TX.jpg" alt="Laura out in West Texas" />
+          <img className="img-portrait" src="https://we-portfolio.s3.amazonaws.com/LAURA_WEST_TX_mobile.jpg" alt="Laura out in West Texas" />
         </div>
       </div>
     );

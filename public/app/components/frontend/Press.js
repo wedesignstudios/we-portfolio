63fd352d75040fc3e24ce3a2502faf9d18f071bb
wedesignstudios <deelden@gmail.com>
2017-08-18 16:45:32 -0500

    add inline-block display rule to card

--- public/app/components/frontend/Press.js
+++ public/app/components/frontend/Press.js
@@ -39,24 +39,22 @@ class Press extends Component {
               <div className="card-columns card-columns-gap-3rem card-columns-gap-2rem card-columns-5 card-columns-2">
                 {newsData.map(story => {
                   return(
-                    <div className="mb-4" key={story.id}>
-                      <div className="card line-height-1-25-rem border-0">
-                      <Link
-                        to={`${this.props.match.url}/${story.slug}`} >
-                          <img
-                            className="card-img-top img-fluid rounded-0"
-                            src={story.image.url}
-                            alt={story.image.alt} />
-                      </Link>
-                        <div className="card-block p-0 pt-3">
-                          <p className="card-title mb-2">
-                            <Link
-                              to={`${this.props.match.url}/${story.slug}`}
-                              className="text-muted">
-                                {story.title}
-                            </Link>
-                          </p>
-                        </div>
+                    <div className="card line-height-1-25-rem border-0 d-inline-block mb-4" key={story.id}>
+                    <Link
+                      to={`${this.props.match.url}/${story.slug}`} >
+                        <img
+                          className="card-img-top img-fluid rounded-0"
+                          src={story.image.url}
+                          alt={story.image.alt} />
+                    </Link>
+                      <div className="card-block p-0 pt-3">
+                        <p className="card-title mb-2">
+                          <Link
+                            to={`${this.props.match.url}/${story.slug}`}
+                            className="text-muted">
+                              {story.title}
+                          </Link>
+                        </p>
                       </div>
                     </div>
                     )

4fbd2eade8f3de1857a475e79de612fd94b92191
wedesignstudios <deelden@gmail.com>
2017-08-20 11:44:26 -0500

    complete initial PressStory layout

--- public/app/components/frontend/PressStory.js
+++ public/app/components/frontend/PressStory.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import 'whatwg-fetch';
 
+const DateFormatter = require('../../services/date_formatter');
+
 class PressStory extends Component {
   constructor() {
     super();
@@ -28,10 +30,36 @@ class PressStory extends Component {
   render() {
     let { newsData } = this.state;
     let { margin } = this.props;
+    let storyDate = new Date(newsData.date);
 
+    if(this.state.newsData.length < 1) return null;
     return(
-      <div id="press-story" style={{marginTop: margin}}>
-        Story: {newsData.title}
+      <div id="press-story" className="row m-0" style={{marginTop: margin}}>
+        <div
+          id="press-feature-image"
+          className="feature-image-container col-12 p-0">
+          <img
+            className="img-fluid"
+            src={newsData.image.url}
+            title={newsData.image.title}
+            alt={newsData.image.alt} />
+        </div>
+        <div id="press-text" className="col-12 container p-0">
+          <div className="row justify-content-center m-0">
+            <div className="col-8 container p-0 mb-5rem mt-5">
+              <h6 className="text-uppercase press-title-letter-spacing line-height-1-45-rem">{newsData.title}</h6>
+              <hr className="hr-1rem" />
+              <div className="row m-0">
+                <div id="press-date" className="col-sm-6 p-0 pr-sm-3">
+                  <p className="font-weight-bold">{DateFormatter.monthYear(storyDate)}</p>
+                </div>
+                <div className="col-sm-6 p-0 pl-sm-3">
+                  <div className="white-space-pre-line">{newsData.description}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

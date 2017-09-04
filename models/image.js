0f52a474ba8f3e771db371ffbd5ee3aaddd1c4ac
wedesignstudios <deelden@gmail.com>
2017-09-04 14:25:21 -0500

    update Image/Project models to integrate ProjectFeatureImage model

--- models/image.js
+++ models/image.js
@@ -2,6 +2,7 @@ const bookshelf = require('../db/bookshelf');
 
 const Project = require('./project');
 const NewsStory = require('./news_story');
+const ProjectFeatureImage = require('./project_feature_images');
 
 const Image = bookshelf.Model.extend({
   tableName: 'images',
@@ -12,6 +13,9 @@ const Image = bookshelf.Model.extend({
   },
   news_story: function() {
     return this.belongsTo('NewsStory', 'news_story_id');
+  },
+  feature_image_project: function() {
+    return this.hasOne('ProjectFeatureImage');
   }
 });
 
--- models/project.js
+++ models/project.js
@@ -3,6 +3,7 @@ const bookshelf = require('../db/bookshelf');
 const Client = require('./client');
 const Collaborator = require('./collaborator');
 const Image = require('./image');
+const ProjectFeatureImage = require('./project_feature_images');
 const ProjectCategory = require('./project_category');
 const ProjectsClients = require('./projects_clients');
 const ProjectsCollaborators = require('./projects_collaborators');
@@ -22,6 +23,9 @@ const Project = bookshelf.Model.extend({
   images: function() {
     return this.hasMany('Image');
   },
+  feature_image: function() {
+    return this.hasOne('ProjectFeatureImage');
+  },
   project_categories: function() {
     return this.belongsToMany('ProjectCategory').through('ProjectsProjectCategories');
   },
@@ -29,7 +33,7 @@ const Project = bookshelf.Model.extend({
     return this.hasOne('ProjectImagesSortOrder');
   }
 }, {
-  dependents: ['clients', 'collaborators', 'images', 'project_categories', 'project_images_sort_order']
+  dependents: ['clients', 'collaborators', 'images', 'project_categories', 'project_images_sort_order', 'feature_image']
 });
 
 module.exports = bookshelf.model('Project', Project);

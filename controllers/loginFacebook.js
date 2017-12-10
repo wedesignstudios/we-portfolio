07718b5a06f010dd06b67a48f6cc7deec925bd24
wedesignstudios <deelden@gmail.com>
2017-12-10 16:47:12 -0600

    define callbackHost var by value of NODE_ENV

--- controllers/loginFacebook.js
+++ controllers/loginFacebook.js
@@ -3,11 +3,15 @@ const passport = require('passport');
 const passportFacebook = require('passport-facebook');
 const router = express.Router();
 const User = require('../models/user');
+const ENV = process.env.NODE_ENV;
+var callbackHost = undefined;
+
+(ENV === 'development') ? (callbackHost = 'http://localhost:3000') : (callbackHost = 'http://we-portfolio.herokuapp.com');
 
 passport.use(new passportFacebook({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: 'http://we-portfolio.herokuapp.com/login/facebook/callback',
+    callbackURL: `${callbackHost}/login/facebook/callback`,
     profileFields: ['id', 'displayName', 'email']
   },
   function(accessToken, refreshToken, profile, cb) {    
@@ -41,7 +45,7 @@ router.get('/', passport.authenticate('facebook', { scope: 'email' }));
 
 router.get('/callback', passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
-    res.header('Access-Control-Allow-Origin', 'http://we-portfolio.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', callbackHost);
     res.header('Access-Control-Allow-Credentials', true);
     req.session.cookie.user = req.user;
     res.redirect('/dashboard');
--- controllers/loginGoogle.js
+++ controllers/loginGoogle.js
@@ -3,11 +3,15 @@ const passport = require('passport');
 const passportGoogle = require('passport-google-oauth').OAuth2Strategy;
 const router = express.Router();
 const User = require('../models/user');
+const ENV = process.env.NODE_ENV;
+var callbackHost = undefined;
+
+(ENV === 'development') ? (callbackHost = 'http://localhost:3000') : (callbackHost = 'http://we-portfolio.herokuapp.com');
 
 passport.use(new passportGoogle({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://we-portfolio.herokuapp.com/login/google/callback'
+    callbackURL: `${callbackHost}/login/google/callback`
   },
   function(token, tokenSecret, profile, cb) {
     User
@@ -41,7 +45,7 @@ router.get('/', passport.authenticate('google', { scope: 'openid email' }));
 
 router.get('/callback', passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    res.header('Access-Control-Allow-Origin', 'http://we-portfolio.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', callbackHost);
     res.header('Access-Control-Allow-Credentials', true);
     req.session.cookie.user = req.user;
     res.redirect('/dashboard');

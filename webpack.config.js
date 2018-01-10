81753f3da5bb5f8fbfb5696a0edc8e541a572f31
wedesignstudios <deelden@gmail.com>
2018-01-10 12:54:03 -0600

    reconfigure package, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       filename: 'public/build/[name].bundle.js',
       sourceMapFilename: 'public/build/[name].bundle.map'
   },
-  devtool: '#source-map',
+  devtool: 'source-map',
   module: {
    loaders: [
      {

06fd19855f6ace588624cc9ebba7e219a8d8e008
wedesignstudios <deelden@gmail.com>
2018-04-09 14:10:40 -0500

    destroy session on logout

--- controllers/logout.js
+++ controllers/logout.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  req.logout();  
+  req.logout();
+  req.session.destroy();
   res.redirect('/login');
 });
 

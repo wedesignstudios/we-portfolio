b37bf7ce75bff2f14791584c355b23521afd88ad
wedesignstudios <deelden@gmail.com>
2018-11-26 18:35:03 +0100

    create prefooter component

---
+++ public/app/components/frontend/PreFooter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class PreFooter extends Component {
+  render() {
+    return (
+      <div id="pre_footer" className="row justify-content-center m-0">
+        <p>Herrow!</p>
+      </div>
+    )
+  }
+}
+
+module.exports = PreFooter;

71679d842f9c67ad0a5e10351b0b07a5cab654f4
wedesignstudios <deelden@gmail.com>
2018-11-27 19:57:35 +0100

    add FormMailchimpSignUp component

---
+++ public/app/components/frontend/FormMailchimpSignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class FormMailchimpSignUp extends Component {
+  render() {
+    return (
+      <div id="mc_signup" className="col-6">
+        <p>Mailchimp</p>
+      </div>
+    )
+  }
+}
+
+module.exports = FormMailchimpSignUp;

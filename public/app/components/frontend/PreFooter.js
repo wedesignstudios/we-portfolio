63cdbc9daad35c3e4ac846f7bd48056e77fb1817
wedesignstudios <deelden@gmail.com>
2019-01-15 13:30:52 -0600

    add canvas and animation to PreFooter

--- public/app/components/frontend/PreFooter.js
+++ public/app/components/frontend/PreFooter.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const preFooterScript = require('../../../javascripts/canvasPreFooter');
 const FormMailchimpSignUp = require('./FormMailchimpSignUp');
 
 class PreFooter extends Component {
   render() {
     return (
       <div id="pre_footer" className="row justify-content-center m-0">
-        <div id="pre_footer_overlay"></div>
+        <div id="pre_footer_overlay">
+          <canvas id="canvas-pre-footer"></canvas>
+        </div>
         <FormMailchimpSignUp />
       </div>
     )
---
+++ public/javascripts/canvasPreFooter.js
@@ -0,0 +1,109 @@
+const canvasPreFooter = function() {
+  window.onload = function() {
+
+    const canvas = document.getElementById('canvas-pre-footer');
+    const ctx = canvas.getContext('2d');
+    const colorArr = ['1, 41, 127',
+                      '79, 135, 255',
+                      '2, 83, 255',
+                      '39, 67, 127',
+                      '2, 66, 204'];
+
+    const colorArr2 = ['1, 41, 127',
+                      '79, 135, 255',
+                      '2, 83, 255',
+                      '230, 227, 26',
+                      '2, 66, 204'];
+
+    var maxRadius = 150;
+
+    canvas.style.width ='100%';
+    canvas.style.height='100%';
+    canvas.width  = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    function getRandomArbitrary(min, max) {
+      let randomNum = Math.random() * (max - min) + min;
+      return Math.floor(randomNum);
+    }
+
+    function randomColor(colorArr) {
+      let index = getRandomArbitrary(0, colorArr.length-1),
+          a = Math.random() * (0.75, 0.15) + 0.15;
+
+      return `rgba(${colorArr[index]}, ${a})`;
+    }
+
+    function minRadius(min) {
+        let radius = Math.random() * 30 + 1;
+
+      return radius > min ? radius : min;
+    }
+
+    function Circle(x, y, dx, dy, radius, color) {
+      this.x = x;
+      this.y = y;
+      this.dx = dx;
+      this.dy = dy;
+      this.radius = radius;
+      this.color = color;
+
+      this.draw = function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+        ctx.fillStyle = color;
+        ctx.fill();
+      }
+
+      this.update = function() {
+        (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) ? (dx = -dx) : null;
+        (this.y + this.radius >= canvas.height || this.y - this.radius <= 0) ?
+          (dy = -dy) : null;
+        this.x += dx;
+        this.y += dy;
+      }
+    }
+
+    function createCircles(numOfCircles) {
+      circleArray = [];
+
+      for (var i = 0; i < numOfCircles; i++) {
+        var radius = minRadius(15);
+        var x = Math.random() * (canvas.width - radius * 2) + radius;
+        var y = Math.random() * (canvas.height - radius * 2) + radius;
+        var dx = (Math.random() - 0.5) * 1.5;
+        var dy = (Math.random() - 0.5) * 1.5;
+
+        circleArray.push(new Circle(x, y, dx, dy, radius, randomColor(colorArr2)));
+      };
+    }
+
+    function animate() {
+      window.requestAnimationFrame(animate);
+      ctx.clearRect(0, 0, innerWidth, innerHeight);
+      circleArray.forEach(circle => {
+        circle.update();
+        circle.draw();
+      });
+    }
+
+    function resizeCanvas(event) {
+      canvas.width  = canvas.offsetWidth;
+      canvas.height = canvas.offsetHeight;
+      // Must be able to cap number of circles depending on screen width
+      createCircles(75);
+    }
+
+    window.addEventListener('resize', resizeCanvas);
+
+    var circleArray = [];
+    // Must be able to cap number of circles depending on screen width
+    createCircles(75);
+    animate();
+  }
+
+}();
+
+module.exports = canvasPreFooter;

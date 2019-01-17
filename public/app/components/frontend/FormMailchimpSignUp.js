010743f95c16ec21b9ae16b87439733e35a9ff6d
wedesignstudios <deelden@gmail.com>
2019-01-17 11:01:36 -0600

    begin adding form functionality

--- public/app/components/frontend/FormMailchimpSignUp.js
+++ public/app/components/frontend/FormMailchimpSignUp.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class FormMailchimpSignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      FNAME: '',
+      email_address: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  submitForm(event) {
+    alert('A name was submitted');
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div id="mc_signup_wrapper" className="col-10">
@@ -17,21 +43,23 @@ class FormMailchimpSignUp extends Component {
                   <input
                     className="mb-0 mr-2rem"
                     type="text"
-                    name="first_name"
-                    /* value="" */
-                    placeholder="First Name" />
+                    name="FNAME"
+                    placeholder="First Name"
+                    value={this.state.FNAME}
+                    onChange={this.handleInputChange} />
 
                   <input
                     className="mb-0 mr-2rem"
                     type="text"
-                    name="email"
-                    /* value="" */
-                    placeholder="Email" />
+                    name="email_address"
+                    placeholder="Email"
+                    value={this.state.email_address}
+                    onChange={this.handleInputChange} />
 
                   <button
                     className="btn btn-primary ml-auto"
-                    /*disabled={this.requiredFieldsBlank}
-                    onClick={(e) => this.submitForm(e)}*/>
+                    // disabled={this.requiredFieldsBlank}
+                    onClick={(e) => this.submitForm(e)}>
                     GO
                   </button>
                 </div>

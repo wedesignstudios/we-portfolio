0aced7651a514dad119bbb3af7aba6daad6d3864
wedesignstudios <david@wedesignstudios.com>
2019-02-07 13:49:58 -0600

    add countries api test

---
+++ tests/controllers/countries.test.js
@@ -0,0 +1,39 @@
+const request = require('supertest'),
+      app = require('../../index'),
+      knex = require('knex'),
+      config = require('../../knexfile'),
+      db = knex(config['test']);
+
+beforeAll(async () => {
+  await db.raw('TRUNCATE TABLE countries');
+  await db.raw('ALTER SEQUENCE countries_id_seq RESTART WITH 1');
+  return db.seed.run(['test']);
+});
+
+describe('GET /countries', () => {
+  it('responds with 200 status code', (done) => {
+    request(app)
+      .get('/api/v1/countries')
+      .expect(200, done);
+  });
+  it('responds with a JSON object', (done) => {
+    request(app)
+      .get('/api/v1/countries')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/, done);
+  });
+  it('responds with a JSON object containing data', (done) => {
+    request(app)
+      .get('/api/v1/countries')
+      .set('Accept', 'application/json')
+      .expect(res => {
+        expect(res.body.length).toBeGreaterThan(0);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        return done();
+      });
+  });
+});

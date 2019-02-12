f5f6f5979e4b7246e8cdc237b35448c46b892043
wedesignstudios <david@wedesignstudios.com>
2019-02-12 12:14:15 -0600

    adjust tab spacing

--- tests/controllers/addresses.test.js
+++ tests/controllers/addresses.test.js
@@ -5,9 +5,9 @@ const config = require('../../knexfile'),
       testGET = require('../helpers/api/httpGET');
 
 beforeAll(async () => {
-   await trunc.truncateCascade(knex, ['addresses']);
-   await idSeq.idSeqReset(knex, 'addresses');
-   return knex('addresses').insert({'city': 'Austin', 'state': 'TX', 'country': 'US'});
+  await trunc.truncateCascade(knex, ['addresses']);
+  await idSeq.idSeqReset(knex, 'addresses');
+  return knex('addresses').insert({'city': 'Austin', 'state': 'TX', 'country': 'US'});
 });
 
 testGET.httpGET('addresses');

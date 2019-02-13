9750a51893afa0c55623385313568937462a42b0
wedesignstudios <david@wedesignstudios.com>
2019-02-13 13:47:46 -0600

    add #httpGETByIdRelated test helper method

--- tests/helpers/api/httpGET.js
+++ tests/helpers/api/httpGET.js
@@ -74,4 +74,31 @@ function httpGETById(routeName, id) {
   )
 }
 
-module.exports = { httpGET, httpGETRelated, httpGETById };
+function httpGETByIdRelated(routeName, id, relatedModelsArr) {
+  return(
+    describe(`GET /${routeName}/${id}`, () => {
+      it('responds with JSON object containing data by id', (done) => {
+        request(app)
+          .get(`/api/v1/${routeName}/${id}`)
+          .set('Accept', 'application/json')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(res => {
+            expect(res.body).toBeInstanceOf(Object);
+            expect(res.body.id).toBe(id);
+            relatedModelsArr.map(model => {
+              return expect(res.body).toHaveProperty(model);
+            })
+          })
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            }
+            return done();
+          });
+      })
+    })
+  )
+}
+
+module.exports = { httpGET, httpGETRelated, httpGETById, httpGETByIdRelated };

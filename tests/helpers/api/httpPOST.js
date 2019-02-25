de8e45484d97a123ea8d7ef666746e358b936400
wedesignstudios <david@wedesignstudios.com>
2019-02-25 13:23:16 -0600

    add test to #httpPOST to check if table count increases

--- tests/helpers/api/httpPOST.js
+++ tests/helpers/api/httpPOST.js
@@ -5,27 +5,44 @@ const request = require('supertest'),
       knex = require('knex')(config['test']),
       trunc = require('../knex/truncate'),
       idSeq = require('../knex/idSeqReset'),
+      tblNm = require('../knex/tableName'),
       login = require('../login/testLogin');
 
 function httpPOST(routeName, data) {
   return(
     describe(`POST /${routeName}`, () => {
+      let table = tblNm.tableName(routeName),
+          tableCount1;
+
       beforeAll(async () => {
         await trunc.truncateCascade(knex, ['users']);
-        await idSeq.idSeqReset(knex, 'users');        
+        await idSeq.idSeqReset(knex, 'users');
+        await knex(table)
+                .count('id')
+                .then(cnt => {
+                  tableCount1 = parseInt(cnt[0]['count']);
+              });
         return knex.seed.run(['test']);
       });
 
       it('login', login.testLogin(agent));
 
-      it('creates a new instance of data model', (done) => {
+      it(`persists a new record in the ${table} db table`, (done) => {
         agent
           .post(`/api/v1/${routeName}`)
           .send(data)
           .expect(200)
-          .end((err, res) => {
-            if (err) return done(err);
-            done();
+          .then(() => {
+            return(
+              knex(table)
+                .count('id')
+                .then(cnt => {
+                  let tableCount2 = parseInt(cnt[0]['count']);
+
+                  expect(tableCount2).toBe(tableCount1 + 1);
+                  done();
+                })
+            )
           });
       });
     })

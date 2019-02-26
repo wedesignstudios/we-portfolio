b1fbe6620f93675aff6c7d7ee9e59217b8f501ab
wedesignstudios <david@wedesignstudios.com>
2019-02-26 14:10:27 -0600

    add #httpDELETE test to clients api test

--- tests/controllers/clients.test.js
+++ tests/controllers/clients.test.js
@@ -5,6 +5,7 @@ const config = require('../../knexfile'),
       testGET = require('../helpers/api/httpGET'),
       testPOST = require('../helpers/api/httpPOST'),
       testPUT = require('../helpers/api/httpPUT'),
+      testDELETE = require('../helpers/api/httpDELETE'),
       relatedModels = ['projects', 'address'],
       postData = {'name': 'POST Client Inc.', 'url': 'https://postclientinc.com'},
       putData = {'name': 'PUT Client LLC', 'url': 'https://putclientinc.com'};
@@ -19,3 +20,4 @@ testGET.httpGETRelated('clients', relatedModels);
 testGET.httpGETByIdRelated('clients', 1, relatedModels);
 testPOST.httpPOST('clients', postData);
 testPUT.httpPUT('clients', 1, putData);
+testDELETE.httpDELETE('clients', 1);

b1fcab27231a7a9ee42bb9bf48ccc77a452bbbe6
wedesignstudios <david@wedesignstudios.com>
2019-02-28 14:04:28 -0600

    remove tear down/set up of news_categories table

--- tests/controllers/newsCategories.test.js
+++ tests/controllers/newsCategories.test.js
@@ -1,14 +1,4 @@
-const config = require('../../knexfile'),
-      knex = require('knex')(config['test']),
-      trunc = require('../helpers/knex/truncate'),
-      idSeq = require('../helpers/knex/idSeqReset'),
-      testGET = require('../helpers/api/httpGET');
-
-beforeAll(async () => {
-  await trunc.truncateCascade(knex, ['news_categories']);
-  await idSeq.idSeqReset(knex, 'news_categories');
-  return knex('news_categories').insert({'name': 'Press Release'});
-});
+const testGET = require('../helpers/api/httpGET');
 
 testGET.httpGET('news-categories');
 testGET.httpGETRelated('news-categories', ['news_story']);

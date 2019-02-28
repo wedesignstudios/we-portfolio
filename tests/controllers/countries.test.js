bd7f3bec632a57eb3ddec2c819aaf2e4f635d041
wedesignstudios <david@wedesignstudios.com>
2019-02-28 14:09:29 -0600

    remove tear down/set up of countries table

--- tests/controllers/countries.test.js
+++ tests/controllers/countries.test.js
@@ -1,13 +1,3 @@
-const config = require('../../knexfile'),
-      knex = require('knex')(config['test']),
-      trunc = require('../helpers/knex/truncate'),
-      idSeq = require('../helpers/knex/idSeqReset'),
-      testGET = require('../helpers/api/httpGET');
-
-beforeAll(async () => {
-  await trunc.truncate(knex, ['countries']);
-  await idSeq.idSeqReset(knex, 'countries');
-  return knex.seed.run(['test']);
-});
+const testGET = require('../helpers/api/httpGET');
 
 testGET.httpGET('countries');

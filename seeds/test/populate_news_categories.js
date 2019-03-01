ffc44f363dcfb658b5cfcb4c32a0ec7d794d4e4e
wedesignstudios <david@wedesignstudios.com>
2019-03-01 12:03:12 -0600

    remove trunc, id seq reset on news_categories seeds

--- seeds/test/populate_news_categories.js
+++ seeds/test/populate_news_categories.js
@@ -1,16 +1,6 @@
-const idSeq = require('../../tests/helpers/knex/idSeqReset'),
-      trunc = require('../../tests/helpers/knex/truncate');
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return trunc.truncateCascade(knex, ['news_categories'])
-    .then(() => {
-      return idSeq.idSeqReset(knex, 'news_categories');
-    })
-    .then(function () {
-      // Inserts seed entries
-      return knex('news_categories').insert([
-        {"name": "Press Release", "created_at": new Date, "updated_at": new Date}, {"name": "Event", "created_at": new Date, "updated_at": new Date}, {"name": "Award", "created_at": new Date, "updated_at": new Date}, {"name": "Media Mention", "created_at": new Date, "updated_at": new Date}
-      ]);
-    });
+  // Inserts seed entries
+  return knex('news_categories').insert([
+    {"name": "Press Release", "created_at": new Date, "updated_at": new Date}, {"name": "Event", "created_at": new Date, "updated_at": new Date}, {"name": "Award", "created_at": new Date, "updated_at": new Date}, {"name": "Media Mention", "created_at": new Date, "updated_at": new Date}
+  ]);
 };

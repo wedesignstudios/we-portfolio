86388a64840ce835dce13d2686cda9e18277a177
wedesignstudios <david@wedesignstudios.com>
2019-03-01 12:41:11 -0600

    remove tear down/set up of addresses table in test

--- tests/controllers/addresses.test.js
+++ tests/controllers/addresses.test.js
@@ -1,13 +1,3 @@
-const config = require('../../knexfile'),
-      knex = require('knex')(config['test']),
-      trunc = require('../helpers/knex/truncate'),
-      idSeq = require('../helpers/knex/idSeqReset'),
-      testGET = require('../helpers/api/httpGET');
-
-beforeAll(async () => {
-  await trunc.truncateCascade(knex, ['addresses']);
-  await idSeq.idSeqReset(knex, 'addresses');
-  return knex('addresses').insert({'city': 'Austin', 'state': 'TX', 'country': 'US'});
-});
+const testGET = require('../helpers/api/httpGET');
 
 testGET.httpGET('addresses');

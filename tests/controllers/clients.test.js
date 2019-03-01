b9db93764f32e668335b5fda02bfed5228c9c540
wedesignstudios <david@wedesignstudios.com>
2019-03-01 12:49:26 -0600

    remove tear down/set up of clients table from test

--- tests/controllers/clients.test.js
+++ tests/controllers/clients.test.js
@@ -1,20 +1,10 @@
-const config = require('../../knexfile'),
-      knex = require('knex')(config['test']),
-      trunc = require('../helpers/knex/truncate'),
-      idSeq = require('../helpers/knex/idSeqReset'),
-      testGET = require('../helpers/api/httpGET'),
+const testGET = require('../helpers/api/httpGET'),
       testPOST = require('../helpers/api/httpPOST'),
       testPUT = require('../helpers/api/httpPUT'),
       testDELETE = require('../helpers/api/httpDELETE'),
       relatedModels = ['projects', 'address'],
       postData = {'name': 'POST Client Inc.', 'url': 'https://postclientinc.com'},
       putData = {'name': 'PUT Client LLC', 'url': 'https://putclientinc.com'};
-
-beforeAll(async () => {
-  await trunc.truncateCascade(knex, ['clients']);
-  await idSeq.idSeqReset(knex, 'clients');
-  return knex('clients').insert({'name': 'My Client LLC', 'url': 'https://myclientllc.com'});
-});
 
 testGET.httpGETRelated('clients', relatedModels);
 testGET.httpGETByIdRelated('clients', 1, relatedModels);

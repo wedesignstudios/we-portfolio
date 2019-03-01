c49d4624dde123cd48317e205b2699fff5c328f8
wedesignstudios <david@wedesignstudios.com>
2019-03-01 13:06:11 -0600

    remove tear down/set up of collaborators table from test

--- tests/controllers/collaborators.test.js
+++ tests/controllers/collaborators.test.js
@@ -1,20 +1,10 @@
-const config = require('../../knexfile'),
-      knex = require('knex')(config['test']),
-      trunc = require('../helpers/knex/truncate'),
-      idSeq = require('../helpers/knex/idSeqReset'),
-      testGET = require('../helpers/api/httpGET'),
+const testGET = require('../helpers/api/httpGET'),
       testPOST = require('../helpers/api/httpPOST'),
       testPUT = require('../helpers/api/httpPUT'),
       testDELETE = require('../helpers/api/httpDELETE'),
       relatedModels = ['projects', 'address'],
       postData = {'name': 'POST Collaborator Inc.', 'url': 'https://postcollaboratorinc.com'},
       putData = {'name': 'PUT Collaborator LLC', 'url': 'https://putcollaboratorinc.com'};
-
-beforeAll(async () => {
-  await trunc.truncateCascade(knex, ['collaborators']);
-  await idSeq.idSeqReset(knex, 'collaborators');
-  return knex('collaborators').insert({'name': 'My Collaborator LLC', 'url': 'https://mycollaboratorllc.com'});
-});
 
 testGET.httpGETRelated('collaborators', relatedModels);
 testGET.httpGETByIdRelated('collaborators', 1, relatedModels);

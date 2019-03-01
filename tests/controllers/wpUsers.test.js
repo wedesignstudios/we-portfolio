73ecbfa4887f34dd99cba2147dc4c218abab7a59
wedesignstudios <david@wedesignstudios.com>
2019-03-01 13:36:02 -0600

    remove tear down/set up of wp_users table from test

--- tests/controllers/wpUsers.test.js
+++ tests/controllers/wpUsers.test.js
@@ -1,13 +1,3 @@
-const config = require('../../knexfile'),
-      knex = require('knex')(config['test']),
-      trunc = require('../helpers/knex/truncate'),
-      idSeq = require('../helpers/knex/idSeqReset'),
-      testGET = require('../helpers/api/httpGET');
-
-beforeAll(async () => {
-  await trunc.truncateCascade(knex, ['wp_users']);
-  await idSeq.idSeqReset(knex, 'wp_users');
-  return knex('wp_users').insert({'user_login': 'david', 'user_email': 'myemail@email.com', 'display_name': 'Full Name'});
-});
+const testGET = require('../helpers/api/httpGET');
 
 testGET.httpGET('wp-users');

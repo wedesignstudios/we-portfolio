20f97a83d928c5df59300aed271493a66a2bb6c9
wedesignstudios <david@wedesignstudios.com>
2019-03-01 12:04:35 -0600

    truncate specified tables before tests run

--- tests/config/globalSetup.js
+++ tests/config/globalSetup.js
@@ -1,6 +1,13 @@
 const config = require('../../knexfile'),
-      knex = require('knex')(config['test']);
+      knex = require('knex')(config['test']),
+      trunc = require('../helpers/knex/truncate'),
+      arrOfTables = [
+        'images',
+        'news_categories',
+        'news_stories'
+      ];
 
 module.exports = async () => {
+  await trunc.truncateCascade(knex, arrOfTables)
   return knex.seed.run(['test']);
 };

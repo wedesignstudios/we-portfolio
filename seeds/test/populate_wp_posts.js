5a097eecffe541965dc19181c9e4ef24cf3ced1a
wedesignstudios <david@wedesignstudios.com>
2019-03-04 10:59:42 -0600

    correct issues with wp posts test seeds

--- seeds/test/populate_wp_posts.js
+++ seeds/test/populate_wp_posts.js
@@ -2,12 +2,12 @@ exports.seed = function(knex, Promise) {
   // Inserts seed entries
   return knex('wp_posts').insert([
     {
-      "post_author": "WP Author",
+      "post_author": "1",
       "post_date": new Date,
       "post_content": "Nulla ligula purus, blandit venenatis pharetra ut, pulvinar et nisi. Mauris tortor metus, rhoncus iaculis ultricies sit amet, imperdiet at ante.",
       "post_title": "WP Post Title",
       "post_excerpt": "WP Excerpt",
-      "post_name": "WP Post Name",
+      "post_name": "wp-post-name",
       "created_at": new Date,
       "updated_at": new Date
    }

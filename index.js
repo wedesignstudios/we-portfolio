742839424a727e83fd2d4238e076378ae97c8793
wedesignstudios <david@wedesignstudios.com>
2019-12-13 11:37:02 -0600

    update outdated deps

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const passport = require('passport');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const knex = require('knex');
 
 // controllers
@@ -102,7 +101,6 @@ app.use(passport.session());
 app.use(favicon(path.join(__dirname, 'public/favicon', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),

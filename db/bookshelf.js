8bb554df455f1322ca23f144d25d0b0d465f1570
wedesignstudios <david@wedesignstudios.com>
2020-09-06 19:41:45 -0500

    remove call to bookshelf registry plugin

--- db/bookshelf.js
+++ db/bookshelf.js
@@ -3,7 +3,6 @@ const knex = require('knex')(config[process.env.NODE_ENV]);
 const bookshelf = require('bookshelf')(knex);
 const cascadeDelete = require('bookshelf-cascade-delete');
 
-bookshelf.plugin('registry');
 bookshelf.plugin(cascadeDelete);
 bookshelf.plugin(require('bookshelf-check-duplicates'));
 

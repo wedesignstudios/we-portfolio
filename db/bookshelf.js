f759ba755adf97ff70914e0287062219d60893b0
wedesignstudios <david@wedesignstudios.com>
2020-12-31 13:23:32 -0600

    revert back to using Bookshelf 'registry' plugin

--- db/bookshelf.js
+++ db/bookshelf.js
@@ -3,6 +3,7 @@ const knex = require('knex')(config[process.env.NODE_ENV]);
 const bookshelf = require('bookshelf')(knex);
 const cascadeDelete = require('bookshelf-cascade-delete');
 
+bookshelf.plugin('registry');
 bookshelf.plugin(cascadeDelete);
 bookshelf.plugin(require('bookshelf-check-duplicates'));
 
